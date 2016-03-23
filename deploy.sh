69f00a7aa8e9d6217351cd8cde6eb74f53285d9a
Alex Bogdanovski <alex@erudika.com>
2016-03-23 19:15:54 +0200

    updated para-admin-ui

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-git commit -am "updated para-admin-ui" && git checkout gh-pages
+git add -A && git commit -am "updated para-admin-ui" && git push origin master
+git checkout gh-pages
+git rebase master
 #cp -Rf dist/* .
-git add -A && git commit -am "updated para-admin-ui"
+#git add -A && git commit -am "updated para-admin-ui"
 git push origin gh-pages
 git checkout master
-git push origin master
 echo "-- done --"
